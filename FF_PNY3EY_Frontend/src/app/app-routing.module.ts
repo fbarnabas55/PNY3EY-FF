d1102e5024482f20dee9d75fbe09896e143cc7ae
fbarnabas55 <fenyvesi.barnabas55@gmail.com>
2026-01-21 21:45:11 +0100

    Add routing for order editing and details in app-routing.module.ts

--- FF_PNY3EY_Frontend/src/app/app-routing.module.ts
+++ FF_PNY3EY_Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderListComponent } from './pages/order-list/order-list.component';
 import { OrderEditorComponent } from './pages/order-editor/order-editor.component';
+import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/orders', pathMatch: 'full'},
   {path: 'orders', component: OrderListComponent},
   {path: "orders/new", component: OrderEditorComponent},
+  { path: 'orders/edit/:id', component: OrderEditorComponent },
+  { path: 'orders/create', component: OrderEditorComponent },
+  { path: 'orders/:id/details', component: OrderDetailsComponent },
 ];
 
 @NgModule({
